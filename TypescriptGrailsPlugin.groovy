f3d0f613e53df51b158a0658b6233ce4cbd8a8b4
Burt Beckwith <burt@burtbeckwith.com>
2013-03-19 23:09:16 -0400

    dropped the minimum Grails version to 2.0

--- TypescriptGrailsPlugin.groovy
+++ TypescriptGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class TypescriptGrailsPlugin {
     // the plugin version
     def version = "0.3"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.2 > *"
+    def grailsVersion = "2.0 > *"
     // resources that are excluded from plugin packaging
     def dependsOn = [resources: '1.0 > *']
     def loadAfter = ['resources']
