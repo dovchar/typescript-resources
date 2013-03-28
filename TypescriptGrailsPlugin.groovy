2ab173949b1711c38266174903dae4a4c2445b52
Dmytro Ovcharenko <o.snich@gmail.com>
2013-03-28 19:03:15 +0200

    change version 0.3.8

--- TypescriptGrailsPlugin.groovy
+++ TypescriptGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TypescriptGrailsPlugin {
-    def version = "0.3.7"
+    def version = "0.3.8"
     def grailsVersion = "2.0 > *"
     def loadAfter = ['resources']
     def title = "TypeScript resource plugin"
