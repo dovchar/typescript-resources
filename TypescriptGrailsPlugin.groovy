1ba8fa200171e90e9c654fb4b13fb54830507425
Dmytro Ovcharenko <o.snich@gmail.com>
2013-04-03 20:17:08 +0300

    change ver for publishing

--- TypescriptGrailsPlugin.groovy
+++ TypescriptGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class TypescriptGrailsPlugin {
-    def version = "0.4"
+    def version = "0.4.1"
     def grailsVersion = "2.0 > *"
     def dependsOn = [resources:'1.0 > *']
     def loadAfter = ['resources']
